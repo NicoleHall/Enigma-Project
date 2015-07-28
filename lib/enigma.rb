489cd877877bc5f9f0e748d46ee3f1adb2efac5a
Adam Ki Jensen <adajensen@gmail.com>
2015-07-28 17:48:52 -0600

    runs from Terminal. Need to add better testing.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,23 +4,44 @@ require_relative "./offset_calc"
 require_relative "./encrypt"
 
 class Enigma
-  def run
-    handle = File.open(ARGV[0], "r")
-    text_input = handle.read.chomp
-    handle.close
 
-    test_encrypt = Encrypt.new
-    test_key = Offset.new
-    encrypted_message = test_encrypt.encrypt_message(text_input, test_key.offset_calculator)
+  attr_accessor :encrypted_message, :input
+
+  def initialize
+    @input = input
+    @encrypted_message = encrypted_message
+  end
+
+  def get_key
+    key = KeyGenerator.new
+    key.randomize_5_digits.join
+  end
+
+  def get_file
+    handle = File.open(ARGV[0], "r")
+    @input = handle.read.chomp
+    handle.close
+  end
+
+  def run_encrypt
+    encrypt_runner = Encrypt.new
+    key_runner = Offset.new
+    @encrypted_message = encrypt_runner.encrypt_message(@input, key_runner.offset_calculator)
+    puts "Created '#{ARGV[1]}' with the key #{get_key} and date #{key_runner.time}."
+  end
+
+  def output
+    writer = File.open(ARGV[1], "w")
+    writer.write(@encrypted_message)
   end
 
 end
 
 
-
-
-
 if __FILE__==$0
   test = Enigma.new
-  puts test.run
+  test.get_key
+  test.get_file
+  test.run_encrypt
+  test.output
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -7,19 +7,20 @@ require "../lib/encrypt"
 class EncryptTest < Minitest::Test
 
   def test_it_can_rotate_one_single_letter_when_given_a_value
-    encrypt_test = Encrypt.new
-    assert_equal "b", encrypt_test.encrypt_character("a", 41)
+    encrypt_runner = Encrypt.new
+    assert_equal "b", encrypt_runner.encrypt_character("a", 41)
   end
 
   def test_it_can_handle_uppercase_letters
-    encrypt_test = Encrypt.new
-    assert_equal "b", encrypt_test.encrypt_character("A", 41)
+    encrypt_runner = Encrypt.new
+    assert_equal "b", encrypt_runner.encrypt_character("A", 41)
   end
 
   def test_it_can_encrypt_two_chars
-    encrypt_test = Encrypt.new
-    assert_equal ["y","g"], encrypt_test.encrypt("du")
+    encrypt_runner = Encrypt.new
+    assert_equal "ed", encrypt_runner.encrypt_message("dd", 41)
   end
+
 
 
 
