ecdb466b5cb65d7eaa06bafcf349d76dbc73a099
Adam Ki Jensen <adajensen@gmail.com>
2015-07-28 22:16:54 -0600

    initial commit

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -4,5 +4,11 @@ require_relative '../lib/decrypt'
 
 class DecryptTest < Minitest::Test
 
+  def test_it_can_get_a_file
+    skip
+    decrypt_runner = Decrypt.new
+    assert decrypt_runner.get_file
+  end
 
+  def test_it_can_
 end
