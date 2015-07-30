fe0ffd17890f3ede8a4766f5b89516f124777b59
Adam Ki Jensen <adajensen@gmail.com>
2015-07-30 02:53:37 -0600

    crack_test.rb added

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,86 +1,90 @@
+require_relative './encrypt'  # => true
+
 class Crack
 
-  template = "nd.."                  # => "nd.."
-  encrypted_message = "x573oz05tzu"  # => "x573oz05tzu"
+  attr_accessor :input, :final_key, :cracked_message  # => nil
 
-  keymap = "abcdefghijklmnlopqrstuvwxyz0123456789,. ".chars.reverse  # => [" ", ".", ",", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "l", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]
+  def initialize(input = get_file)
+    @input = input                                                      # => "ucuuvhuo"
+    @keymap = "abcdefghijklmnlopqrstuvwxyz0123456789,. ".chars.reverse  # => [" ", ".", ",", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "l", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]
+    @template = "nd.."                                                  # => "nd.."
+    @rotation_offset = 0                                                # => 0
+    @final_key = final_key                                              # => nil
+    @cracked_message = cracked_message                                  # => nil
+  end
 
-  last_four = encrypted_message[-4,4].chars     # => ["5", "t", "z", "u"]
-  rotation_offset = encrypted_message.size % 4  # => 3
-  if rotation_offset == 1                       # => false
-    rotation_offset = 3
-  elsif rotation_offset == 3                    # => true
-    rotation_offset = 1                         # => 1
-  end                                           # => 1
+  def get_file
+    incoming_file = File.open(ARGV[0], "r")
+    @input = incoming_file.read.chomp
+    incoming_file.close
+    # require "pry";binding.pry
+  end
 
-rotation_offset                     # => 1
-  # isolate the last 4
-  template = template.chars         # => ["n", "d", ".", "."]
-  # split the last 4 figures
-  # pair with corresponding values
-  paired = last_four.zip(template)  # => [["5", "n"], ["t", "d"], ["z", "."], ["u", "."]]
-  # find the difference of each pair
-  the_one = paired.map do |pair|    # => [["5", "n"], ["t", "d"], ["z", "."], ["u", "."]]
+  def last_four
+    last_four = @input[-4, 4].chars  # => ["v", "h", "u", "o"], ["v", "h", "u", "o"]
+  end
 
-    start = keymap.index(pair.first)      # => 7, 19, 13, 18
-    ending = keymap.index(pair.last)      # => 26, 36, 1, 1
-    if start > ending                     # => false, false, true, true
-      -(keymap.size - (start - ending ))  # => 28, 23
-    else
-     -(ending - start)                    # => -19, -17
-    end                                   # => -19, -17, -28, -23
-  end                                     # => [-19, -17, -28, -23]
-  the_one.rotate(rotation_offset)         # => [-17, -28, -23, -19]
-  # !!! figure out where ABCD spots are.
-  # multiply key vals by -1
-  # run (string , key) through encrypt.rb
+  def set_rotation_offset
+
+    @rotation_offset = @input.size % 4  # => 0
+    if @rotation_offset == 1            # => false
+      @rotation_offset = 3
+    elsif @rotation_offset == 3         # => false
+      @rotation_offset = 1
+    end                                 # => nil
+  end
+
+  def set_pairs
+    @template = @template.chars         # => ["n", "d", ".", "."]
+    @paired = last_four.zip(@template)  # => [["v", "n"], ["h", "d"], ["u", "."], ["o", "."]]
+
+  end
+
+  def find_rotation_distance
+    @final_key = @paired.map do |pair|       # => [["v", "n"], ["h", "d"], ["u", "."], ["o", "."]]
+      start = @keymap.index(pair.first)      # => 17, 32, 18, 24
+      ending = @keymap.index(pair.last)      # => 26, 36, 1, 1
+      if start > ending                      # => false, false, true, true
+        -(@keymap.size - (start - ending ))  # => 23, 17
+      else
+       -(ending - start)                     # => -9, -4
+      end                                    # => -9, -4, -23, -17
+    end                                      # => [-9, -4, -23, -17]
+    @final_key.rotate(@rotation_offset)      # => [-9, -4, -23, -17]
+  end
+
+
+
+
+  def send_to_encrypt_class
+    encrypt_runner = Encrypt.new                                           # => #<Encrypt:0x007f80f508ff70 @encrypted_msg=[], @keymap=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "l", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", " "]>
+    @cracked_message = encrypt_runner.encrypt_message(@input, @final_key)  # => "m..end.."
+  end
+
+  def write_file
+    writer = File.open(ARGV[1], "w")  # ~> TypeError: no implicit conversion of nil into String
+    writer.write(@cracked_message)
+    writer.close
+  end
 end
 
-crack_runner = Crack.new  # => #<Crack:0x007ff2040882b8>
 
+if __FILE__==$0                         # => true
+  crack_runner = Crack.new("ucuuvhuo")  # => #<Crack:0x007f80f509c428 @input="ucuuvhuo", @keymap=[" ", ".", ",", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "l", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"], @template="nd..", @rotation_offset=0, @final_key=nil, @cracked_message=nil>
+  crack_runner.last_four                # => ["v", "h", "u", "o"]
+  # crack_runner.get_file
+  crack_runner.set_rotation_offset      # => nil
+  crack_runner.set_pairs                # => [["v", "n"], ["h", "d"], ["u", "."], ["o", "."]]
+  crack_runner.find_rotation_distance   # => [-9, -4, -23, -17]
+  # crack_runner.shift_key_to_ABCD_order                   # => [-16, -15, -36, -6]
+  crack_runner.send_to_encrypt_class    # => "m..end.."
+  crack_runner.write_file
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 1.read encrypted string in backwards
-# 2. isolate the first four characters
-# 3. index of the first four encrypted characters (relative to character map)
-# 4. need index of the "..end.." of unencrypted message ("..dn")
-# 5. subtract index of encrypted from the unencrypted
-# 6. run the reversed encrypted message through the encryptor with the differences (i.e. key)
-# 7. reverse your unencrypted string
-#find index differences
+# ~> TypeError
+# ~> no implicit conversion of nil into String
+# ~>
+# ~> /Users/adamki/Turing/module1/week_3/enigma2/lib/crack.rb:65:in `initialize'
+# ~> /Users/adamki/Turing/module1/week_3/enigma2/lib/crack.rb:65:in `open'
+# ~> /Users/adamki/Turing/module1/week_3/enigma2/lib/crack.rb:65:in `write_file'
+# ~> /Users/adamki/Turing/module1/week_3/enigma2/lib/crack.rb:81:in `<main>'
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,36 +1,36 @@
-require_relative "./key_generator"  # => true
+require_relative "./key_generator"
 
 class Encrypt
 
-  attr_accessor :encrypted_msg, :keymap  # => nil
+  attr_accessor :encrypted_msg, :keymap
 
   def initialize
-    @encrypted_msg = []                                         # => []
-    @keymap = "abcdefghijklmnlopqrstuvwxyz0123456789,. ".chars  # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "l", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", " "]
+    @encrypted_msg = []
+    @keymap = "abcdefghijklmnlopqrstuvwxyz0123456789,. ".chars
   end        # 0123456789012345678901234567890123456789
 
   def encrypt_message(string, rotation_collection)
     # string.gsub!(/[^0-9a-z]/, '')                                                    # => "hellomomend"
-    index = 0                                                                        # => 0
-    string.chars.each do |character|                                                 # => ["x", "5", "7", "3", "o", "z", "0", "5", "t", "z", "u"]
-      encrypted_msg << encrypt_character(character, rotation_collection[index % 4])  # => ["h"], ["h", "e"], ["h", "e", "l"], ["h", "e", "l", "l"], ["h", "e", "l", "l", "."], ["h", "e", "l", "l", ".", "."], ["h", "e", "l", "l", ".", ".", "e"], ["h", "e", "l", "l", ".", ".", "e", "n"], ["h", "e", "l", "l", ".", ".", "e", "n", "d"], ["h", "e", "l", "l", ".", ".", "e", "n", "d", "."], ["h", "e", "l", "l", ".", ".", "e", "n", "d", ".", "."]
-      index += 1                                                                     # => 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-    end                                                                              # => ["x", "5", "7", "3", "o", "z", "0", "5", "t", "z", "u"]
-    encrypted_msg.join                                                               # => "hell..end.."
+    index = 0
+    string.chars.each do |character|
+      encrypted_msg << encrypt_character(character, rotation_collection[index % 4])
+      index += 1
+    end
+    encrypted_msg.join
   end
 
   def encrypt_character(character, rotation)
-    pos_of_char = keymap.index(character.downcase)               # => 24, 32, 34, 30, 15, 26, 27, 32, 20, 26, 21
-    new_encrypted_char = (pos_of_char + rotation) % keymap.size  # => 7, 4, 11, 11, 38, 38, 4, 13, 3, 38, 38
-    keymap[new_encrypted_char]                                   # => "h", "e", "l", "l", ".", ".", "e", "n", "d", ".", "."
+    pos_of_char = keymap.index(character.downcase)
+    new_encrypted_char = (pos_of_char + rotation) % keymap.size
+    keymap[new_encrypted_char]
   end
 
 end
 
 
-if __FILE__==$0                                               # => true
-  test = Encrypt.new                                          # => #<Encrypt:0x007fc44c09bda8 @encrypted_msg=[], @keymap=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "l", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", " "]>
-  test_key = KeyGenerator.new                                 # => #<KeyGenerator:0x007fc44c09a610 @random=[7, 2, 8, 3, 4], @rotations={:a=>72, :b=>28, :c=>83, :d=>34}>
+if __FILE__==$0
+  test = Encrypt.new
+  test_key = KeyGenerator.new
   #test.encrypt_message('d2v1l',[-18, -34, -26, -10])
-  test.encrypt_message("x573oz05tzu",  [-17, -28, -23, -19])  # => "hell..end.."
-end                                                           # => "hell..end.."
+  # test.encrypt_message("dv,rlpxljuxf",  [-36, -58, -66, -47])
+end
